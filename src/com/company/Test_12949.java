3121a1fc42f362b6de079fd2415f1437a1b1707a
hohoseohyun <hohoseohyun@gmail.com>
2021-09-20 23:45:27 +0900

    프로그래머스 Level 2 - 행렬의 곱셈 (Java)

---
+++ src/com/company/Test_12949.java
@@ -0,0 +1,23 @@
+package com.company;
+
+class Test_12949 {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                for (int k = 0; k < arr1[i].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Test_12949 obj = new Test_12949();
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+        System.out.println(obj.solution(arr1, arr2));
+    }
+}
